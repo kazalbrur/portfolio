648590cdebd234b0b07f99227b5efe4e5cc0f260
Kazal Chanra Barman <kcb.brurcse42@gmail.com>
2025-05-31 18:15:39 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, "src", "styles")],
+    includePaths: [path.join(__dirname, "src","public", "styles")],
   },
-  assetPrefix: '/portfolio/',
 };
